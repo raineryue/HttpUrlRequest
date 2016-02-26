a0ab17b0654b19ead23b8a315b06b67f21d080a1
raineryue <jinlei_yue@sina.com>
2016-02-26 14:15:57 +0800

    Initial Commit

---
+++ HttpUrlRequest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HttpUrlRequest
+//
+//  Created by Rainer on 16/2/26.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HttpUrlRequest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HttpUrlRequest
+//
+//  Created by Rainer on 16/2/26.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
